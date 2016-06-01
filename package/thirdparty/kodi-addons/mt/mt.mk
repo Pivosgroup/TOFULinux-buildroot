c1c583a4dd228d7b76cfcc2eea9f4f1ed217557a
Ahmed Soror <aasoror@hotmail.com>
2016-06-01 04:22:58 -0400

    [addons] update TOFULinux maintenance tool

--- package/thirdparty/kodi-addons/mt/mt.mk
+++ package/thirdparty/kodi-addons/mt/mt.mk
@@ -3,9 +3,9 @@
 ## TOFULinux Maintenance Tool Addon
 #
 ###############################################################################
-MT_VERSION = baaa5a3774a1cb27f60b245c82df119b14866c21
+MT_VERSION = 6662aeb06d84f45b243dd2db5552fcba8db29cc2
 MT_SOURCE = mt-$(MT_VERSION).tar.gz
-MT_SITE = https://github.com/snappy46/TOFULinux-Maintenance-Tool.git
+MT_SITE = https://github.com/aasoror/TOFULinux-Maintenance-Tool.git
 MT_SITE_METHOD = git
 MT_INSTALL_STAGING = NO
 MT_INSTALL_TARGET = YES
